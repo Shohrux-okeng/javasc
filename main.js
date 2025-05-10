d1447b1520bd6e8e1ed5c505ebcc179de76e062a
AlamovShohrux <you@example.com>
2025-05-10 13:41:19 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,100 @@
+1
+// let arr = [1, 2, 3];
+// let kv = [];
+// arr.forEach(function(num) {
+//   kv.push(num * num);
+// }); 
+// console.log(kv);
+
+// let arr = [1, 2, 3];
+// let kv = [];
+// let i = 0;
+// while (i < arr.length) {
+//   kv.push(arr[i] * arr[i]);
+//   i++;
+// }
+// console.log(kv);
+
+
+2
+// let names = ["John", "Akmal"];
+// let okay = [];
+// names.forEach(function(name) {
+//     okay.push("Mr " + name);
+// });
+// console.log(okay);
+
+
+// let names = ["John", "Akmal"];
+//  let okay = [];
+//  let i = 0;
+//  while (i < names.length) {
+//     okay.push("Mr" + names[i]);
+//     i++;
+//  }
+//  console.log(okay);
+
+
+3
+// let numbers = [1, 2, 3, 4];
+// let okay = [];
+// numbers.forEach(function(num) {
+//   if (num % 2 !== 0) {
+//     okay.push(num);
+//   }
+// });
+// console.log(okay);
+
+
+// let numbers = [1, 2, 3, 4];
+// let okay = [];
+// for (let i = 0; i < numbers.length; i++) {
+//   if (numbers[i] % 2 !== 0) {
+//     okay.push(numbers[i]);
+//   }
+// }
+// console.log(okay);
+
+
+4
+// let numbers = [1, 2, 3, 4];
+// let sum = 0;
+// numbers.forEach(function(num) {
+//   if (num % 2 === 0) {
+//     sum += num;
+//   }
+// });
+// console.log(sum);
+
+
+  
+// let numbers = [1, 2, 3, 4];
+// let sum = 0;
+// let i = 0;
+// while (i < numbers.length) {
+//   if (numbers[i] % 2 === 0) {
+//     sum += numbers[i];
+//   }
+//   i++;
+// }
+// console.log(sum);
+
+
+5
+// const arr = ["Lorem", "hi"];
+// const a = [];
+// arr.forEach(function (item) {
+//   a.push(item.length);
+// });
+
+// console.log(a);'
+
+
+// const arr = ["Lorem", "hi"];
+// const a = [];
+// let i = 0;
+// while (i < arr.length) {
+//   a.push(arr[i].length);
+//   i++;
+// }
+// console.log(a);
